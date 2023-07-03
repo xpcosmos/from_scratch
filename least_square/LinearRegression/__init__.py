2f5aa6825b5e2cc062a6a514c37de278e2baffe5
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-03 20:17:12 -0300

    Ajuste na escrita de código

--- least_square/LinearRegression/__init__.py
+++ least_square/LinearRegression/__init__.py
@@ -1,18 +1,13 @@
 import numpy as np
 
 class LinearRegression:
-    
 
     def _set_coef(self):
         return ((self.n*sum(self.X*self.y)) - (sum(self.X) * sum(self.y)))/ (self.n*sum(self.X**2) - sum(self.X)**2)
-        
-        
+ 
     def _set_intercept(self):
         return sum(self.y) - ((self.coef_ * sum(self.X)) / self.n)
 
-    
-    
-    
     
     def fit(self, X, y):
         self.X = X
@@ -21,21 +16,7 @@ class LinearRegression:
         
         self.coef_ = self._set_coef()
         self.intercept_ = self._set_intercept()
-        
-        
-        
-        
-        
-        
-        
-        
+
     def predict(self, X):
         return (self.coef_*X) + self.intercept_
     
-lr = LinearRegression()
-lr.fit(np.array([2,2,3]), np.array([2,5,6]))
-print(lr.predict(np.array([2,5])))
-
-# y = m*x + b
-# m = n*sum(xy) -sum(x)*sum(y)/ n*sum(x**2) - sum(x)**2
-# b = sum(y) - m*sum(x) / n
