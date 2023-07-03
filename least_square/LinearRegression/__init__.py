1e5caabdf4aa2bddf9659f45bb9e4ca00ee3e9fd
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-03 18:16:35 -0300

    Criação de variáveis para método

--- least_square/LinearRegression/__init__.py
+++ least_square/LinearRegression/__init__.py
@@ -1,5 +1,30 @@
-class LinearRegression(self):
-    pass
+class LinearRegression:
+    def __init__(self):
+        # self.X 
+        # self._coef
+        # self._intercept
+        pass
+    
+    
+    @property
+    def X(self):
+        pass
+
+    @property
+    def coef(self):
+        pass
+    
+    @property
+    def intercept(self):
+        pass
+    
+    def fit():
+        pass
+    def predict():
+        pass
+    
+    
+    
 
 # y = m*x + b
 # m = n*sum(xy) -sum(x)*sum(y)/ n*sum(x**2) - sum(x)**2
