6b281a6657f2e0848e705699b42b5dd3cf268334
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-08 15:36:00 -0300

    Definindo K=3 para o padrão

--- from_scratch/neighbors/_kneighborsClassifier.py
+++ from_scratch/neighbors/_kneighborsClassifier.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class KNeighborsClassifier:
-    def __init__(self, K, normalize = False, encoding = None):
+    def __init__(self, K=3, normalize = False, encoding = None):
         self.K = K
         self.normalize = normalize # MinMaxScaler, StandardScaler
         self.encoding = encoding  # OneHotEncoder, OrdinalEncoder
