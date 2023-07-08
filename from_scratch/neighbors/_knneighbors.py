f83ff8355d3766f4eebf93befb9be2b0b6822824
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-08 14:52:28 -0300

    Criação de pasta para módulo de pré-processamento

--- from_scratch/neighbors/_knneighbors.py
+++ from_scratch/neighbors/_knneighbors.py
@@ -1,4 +1,9 @@
 import numpy as np
 
 class KNNeighbors:
-    pass
+    def __init__(self, K, normalize = False, encoding = None):
+        self.K = K
+        self.normalize = normalize # MinMaxScaler, StandardScaler
+        self.encoding = encoding  # OneHotEncoder, OrdinalEncoder
+    
+    
