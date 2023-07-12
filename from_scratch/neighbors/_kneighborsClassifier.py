81ca6dcd9a13797abc17b92435779b16390c389a
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-12 15:52:54 -0300

    Criação de função com distância euclidiana

--- from_scratch/neighbors/_kneighborsClassifier.py
+++ from_scratch/neighbors/_kneighborsClassifier.py
@@ -1,4 +1,9 @@
 import numpy as np
+from collections import Counter
+
+def _distancia_euclidiana(x1, x2):
+        distancia = np.sqrt(np.sum(x1 - x2) ** 2)
+        return distancia
 
 class KNeighborsClassifier:
     def __init__(self, K=3, normalize = False, encoding = None):
@@ -7,6 +12,8 @@ class KNeighborsClassifier:
         self.encoding = encoding  # OneHotEncoder, OrdinalEncoder
     
     def fit(self, X, y):
+        self.X = X
+        self.y = y
         pass
     
     def predict(self, X):
@@ -14,4 +21,13 @@ class KNeighborsClassifier:
         return np.array(predicted_label)
 
     def _predict(self, x):
-        pass
+        # Calculo de distâncias
+        prediction = [_distancia_euclidiana(x, x2) for x2 in self.X]
+        # Extrair kn de amostras e labels
+        k_id = np.argsort(prediction)[:self.K]
+        knn_labels = [self.y[i] for i in k_id]
+        # Labels mais comuns
+        most_comuns = Counter(knn_labels).most_common(1)
+        return most_comuns[0][0]
+
+    
