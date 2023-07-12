2379ffd0726b2e561d3b445c553f688d57dc40fa
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-12 18:25:17 -0300

    Criando datasets para tests

---
+++ from_scratch/datasets/wine.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def wine_dataset():
+    return pd.read_csv('from_scratch/datasets/data/WineQT.csv')
--- from_scratch/test/test_knn.py
+++ from_scratch/test/test_knn.py
@@ -3,5 +3,8 @@ import sys
 sys.path.append(r'.')
 
 from from_scratch.neighbors import KNeighborsClassifier
+from from_scratch.datasets.wine import wine_dataset
 
+df = wine_dataset()
+print(df)
 KNeighborsClassifier()
