8efe3e0424b5556a7f6f9884fb64ef7562bc9619
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-13 13:38:15 -0300

    Criação de estrutura de DecisionTreeClassifier

--- from_scratch/tree/_decisiontreeclassifier.py
+++ from_scratch/tree/_decisiontreeclassifier.py
@@ -2,20 +2,11 @@ import numpy as np
 
 class DecisionTreeClassifier:
     
-    #   Construção de uma árvore de decisão
-    #   *   Escolher atributo
-    #   *   Estender árvore adicionando ramos de acordo com a partição
-    #   *   Passar exemplos para nós
-    #   *   Avaliar critério de parada
-    
-    #   Critério de utilidade de atributo para classificação, ou seja, os atributos devem ser capazes de discriminar as classes
-    #   *   Uma divisão que mantêm proporções de classes em todas as partições é inútil 
-    #   *   Uma divisão onde cada partição todos os exemplos são da mesma classe tem utilidade máxima.
-    
-    #   Medida de partição utiliza o método de ganho de informação e índice GINI.
-    #   A entropia é a medida de aleatoriedade (grau de pureza de uma variável). O ganho de informação mede a redução
-    # da entropia causada pela partição dos exemplos de acordo com os valores do atributo. A entropia de uma variável qualitativa X, com instâncias
-    # pertencentes à classe i, com probabilidade pi, é dada por:  
-    #                                                            Entropia(X) =  Somatório (pi log2 pi)
-     def __init__(self):
+    def __init__(self):
           pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
