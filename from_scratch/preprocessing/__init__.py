2f8c65331e150516664fb8066ec257830c676d4f
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-13 11:26:30 -0300

    Criação de StandardScaler

--- from_scratch/preprocessing/__init__.py
+++ from_scratch/preprocessing/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from from_scratch.preprocessing._minmaxscaler import MinMaxScaler
+from from_scratch.preprocessing._standardscaler import StandardScaler
---
+++ from_scratch/preprocessing/_standardscaler.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class StandardScaler:
+    
+    def fit_transform(self, X_to_transform):
+        
+        X_to_transform = X_to_transform.T
+        
+        for i in range(0, X_to_transform.shape[0]):
+        
+            for j in X_to_transform[i]:
+                row = [self._transform(v, X_to_transform[i]) for v in X_to_transform[i]]
+        
+            X_to_transform[i] = row
+        return X_to_transform.T
+                
+            
+    def _transform(self, x, X_main):
+        x = (x - np.mean(X_main) ) / np.std(X_main)
