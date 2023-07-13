34fc9c0832d6b9e7885e0f3abdba08a130cbeb4c
xpcosmos <mikeias.d.s.o@gmail.com>
2023-07-13 12:08:58 -0300

    Criação de test para regressão linear

---
+++ from_scratch/test/test_linearregression.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append(r'.')
+
+import numpy as np
+from from_scratch.linear_model import LinearRegression
+
+X = [1,2,3,4,5,6,7]
+y = [1.5, 3.8, 6.7, 9.0, 11.2, 13.6, 16]
+
+X = np.array(X)
+X_test = X + np.random.random(np.size(X))
+
+model = LinearRegression().fit(X,y)
+
+print(model.predict(X_test))
+print(model.score(X,y))
